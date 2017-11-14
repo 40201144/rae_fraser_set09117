7739c09e254f7fa587a45b2657b02ad5673c8d1e
40201144 <fraserrae117@gmail.com>
2017-11-14 15:33:24 +0000

    Movement started

--- checkers.py
+++ checkers.py
@@ -11,49 +11,56 @@ bq = 'B'
 
 PLAYERS = Enum("Players", "White Black")
 
-def init_grid():
-    """Initialise the new grid"""
-    grid = [[ey, bp, ey, bp, ey, bp, ey, bp],
-            [bp, ey, bp, ey, bp, ey, bp, ey],
-            [ey, bp, ey, bp, ey, bp, ey, bp],
-            [bp, ey, bp, ey, bp, ey, bp, ey],
-            [ey, ey, ey, ey, ey, ey, ey, ey],
-            [ey, ey, ey, ey, ey, ey, ey, ey],
-            [ey, wp, ey, wp, ey, wp, ey, wp],
-            [wp, ey, wp, ey, wp, ey, wp, ey],
-            [ey, wp, ey, wp, ey, wp, ey, wp],
-            [wp, ey, wp, ey, wp, ey, wp, ey]]
-    return grid
-
 def main():
     """ Entry point """
     print("PY-CHECKERS")
     value_package = dict([("board", init_grid()),("cur_turn", PLAYERS.White)])
     board = init_grid()
-    for i in range(8):
-        move(value_package, board)
-        print_board(board)
-        break
+    while True:
+      print_board(board)
+      move(value_package, board)
+      break
+
+def init_grid():
+    """Initialise the new grid"""
+    grid = [[ey, bp, ey, bp, ey, bp, ey, bp],
+            [bp, ey, bp, ey, bp, ey, bp, ey],
+            [ey, bp, ey, bp, ey, bp, ey, bp],
+            [ey, ey, ey, ey, ey, ey, ey, ey],
+            [ey, ey, ey, ey, ey, ey, ey, ey],
+            [wp, ey, wp, ey, wp, ey, wp, ey],
+            [ey, wp, ey, wp, ey, wp, ey, wp],
+            [wp, ey, wp, ey, wp, ey, wp, ey]]
+    return grid
     
 def print_board(board):
-    print("\n      A B C D E F G H")
-    for i in range(8):
+    print("\n      0 1 2 3 4 5 6 7")
+    for i in range(GRID_HEIGHT):
         print(i, "   |", end="")
-        for j in range (8):
-            print (board[i][j] + "|", end="")
+        for j in range(GRID_WIDTH):
+            print(board[i][j] + "|", end="")
         print("")
     print("")
+
+
 
 def move(value_package, board):
     if value_package["cur_turn"] == PLAYERS.White:
         print("White's turn:\n")
         print_board(board)
+        
+        init_x = int(input("Please choose target x-coordinate: "))
+        init_y = int(input("Please choose target y-coordinate: "))
+        end_x = int(input("Please choose a destination x-coordinate: "))
+        end_y = int(input("Please choose a destination y-coordinate: "))
         while True:
             print("Enter movement :", end="")
             print_board(board)
-            return move(value_package, board)
-    else:
-        print("Black's turn:\n")
-        value_package["cur_turn"] = PLAYERS.White
+        else:
+            print("Black's turn:\n")
+            value_package["cur_turn"] = PLAYERS.White
+
+
+
 
 main()
